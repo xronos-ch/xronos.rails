abce34a7437472594ab0f142852df1f7b665e9a7
nevrome <clemens@nevrome.de>
2019-06-11 13:00:57 +0200

    added leaflet map and menu on welcome page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require leaflet
 //= require_tree .
+
---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,3 @@
+Leaflet.tile_layer = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+Leaflet.attribution = "https://www.openstreetmap.org/copyright"
+Leaflet.max_zoom = 18
