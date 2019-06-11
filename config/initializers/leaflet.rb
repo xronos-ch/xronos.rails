c8d1b373ad42bf76e3efa452c581af1902d3cf17
nevrome <clemens@nevrome.de>
2019-06-11 14:03:43 +0200

    visuals

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,3 @@
-Leaflet.tile_layer = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-Leaflet.attribution = "https://www.openstreetmap.org/copyright"
+Leaflet.tile_layer = "https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png"
+Leaflet.attribution = 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> | Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 Leaflet.max_zoom = 18
