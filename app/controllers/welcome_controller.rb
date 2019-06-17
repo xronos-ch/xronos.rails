5d6b816405a19a0148acca0ee28698c1ef84e1bd
nevrome <clemens@nevrome.de>
2019-06-17 14:57:02 +0200

    more experiments with search and filter infrastructure

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,12 @@ class WelcomeController < ApplicationController
   def index
   end
   def index
-    #@selected_measurements = Site.where("lat > ?", 5).all
-		@selected_measurements = Site.where(name: params[:query_site_name]).all
+		@selected_measurements = Site.where(
+      "name = ? OR (lat >= ? AND lat <= ?)",
+      #"name LIKE '%?%' OR (lat >= ? AND lat <= ?)", 
+      params[:query_site_name],
+      params[:query_lat_start],
+      params[:query_lat_stop],
+    ).all
   end 
 end
