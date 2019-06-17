00c5ff54738547f4e5a7293c799c5ebb33ad751a
nevrome <clemens@nevrome.de>
2019-06-17 14:35:26 +0200

    after long struggle: managed to implement a simple search and filter system

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
   end
   def index
-    @selected_measurements = Site.where("lat > ?", 5).all
+    #@selected_measurements = Site.where("lat > ?", 5).all
+		@selected_measurements = Site.where(name: params[:query_site_name]).all
   end 
 end
