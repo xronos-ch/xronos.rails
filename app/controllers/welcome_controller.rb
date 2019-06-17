aa95dcf69d10cb02130e3e9fcafbb54f59d41c7b
nevrome <clemens@nevrome.de>
2019-06-17 17:37:39 +0200

    brought filtering back to live

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,6 @@ class WelcomeController < ApplicationController
   def index
   end
   def index
-		#@selected_measurements = Site.where(
-      #"name = ? OR (lat >= ? AND lat <= ?)",
-      ##"name LIKE '%?%' OR (lat >= ? AND lat <= ?)", 
-      #params[:query_site_name],
-      #params[:query_lat_start],
-      #params[:query_lat_stop],
-    #).all
     @selected_measurements = Measurement.joins(
       sample: {arch_object: :site}
     ).select(
@@ -17,6 +10,11 @@ class WelcomeController < ApplicationController
       sites.name as site_name, 
       sites.lat as site_lat, 
       sites.lng as site_lng"
+    ).where(
+      "name = ? OR (lat >= ? AND lat <= ?)", ##"name LIKE '%?%' OR (lat >= ? AND lat <= ?)", 
+      params[:query_site_name],
+      params[:query_lat_start],
+      params[:query_lat_stop],
     ).all
   end 
 end
