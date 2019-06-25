7fab76f378dc00c73217cb2dac04bbdcbab286aa
nevrome <clemens@nevrome.de>
2019-06-25 18:02:46 +0200

    added flash message popups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { render nothing: true, status: :not_found }
-      format.html { redirect_to main_app.root_url, alert: exception.message, status: :not_found }
+      format.html { redirect_to request.referrer, alert: exception.message}
       format.js   { render nothing: true, status: :not_found }
     end
   end
