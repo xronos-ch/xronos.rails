a084260dac39ab3cf8ec93a1d40c338189ba79fc
nevrome <clemens@nevrome.de>
2019-06-25 16:42:39 +0200

    catching access denied errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # access management error handling (messages)
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render nothing: true, status: :not_found }
+      format.html { redirect_to main_app.root_url, alert: exception.message, status: :not_found }
+      format.js   { render nothing: true, status: :not_found }
+    end
+  end
+
 end
