fc2c3ef7ad781c56c55d82b1aaa8df2d99f4ae27
nevrome <clemens@nevrome.de>
2019-07-02 15:20:20 +0200

    after struggling since yesterday I finally found a solution to the datatables update problem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,24 +1,30 @@
 class WelcomeController < ApplicationController
-  def index
-  end
 
   def index
 
-    param_spatial_lasso_selection = params[:spatial_lasso_selection];
-    param_query_lat_start = params[:query_lat_start];
-    param_query_lat_stop = params[:query_lat_stop];
+    #### update session ####
+    if params[:query_lat_start].present?
+      session[:query_lat_start] = params[:query_lat_start]
+    end
+    if params[:query_lat_stop].present?
+      session[:query_lat_stop] = params[:query_lat_stop]
+    end
 
     spatial_lasso_selection = Array.new;
-		unless param_spatial_lasso_selection.nil?
-			spatial_lasso_selection = JSON.parse(param_spatial_lasso_selection);
+		unless params[:spatial_lasso_selection].nil?
+			spatial_lasso_selection = JSON.parse(params[:spatial_lasso_selection]);
 		end   
+
+    puts("#####" + params[:query_lat_start].to_s + "#####")
+    #session[:query_lat_start] = params[:query_lat_start]
+    puts("#####" + session[:query_lat_start].to_s + "#####")
 
 		@selected_measurements = Measurement.joins(
       sample: {arch_object: [{site: [:site_type, :country]}, {on_site_object_position: :feature_type}, :material, :species]}
     ).select(
       "
       measurements.labnr as labnr,
-			measurements.year as year,
+      measurements.year as year,
       sites.name as site,
       site_types.name as site_type,
       sites.lat as lat,
@@ -31,8 +37,8 @@ class WelcomeController < ApplicationController
     ).where(
       "lat >= ? AND lat <= ?",
 			#params[:query_site_name],
-      1,#param_query_lat_start,
-      20#param_query_lat_stop,
+      session[:query_lat_start],
+      session[:query_lat_stop],
     ).all
 
 		gon.selected_measurements = @selected_measurements.to_json
