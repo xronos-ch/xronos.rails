7bf9f3ae3aa57adc113a515f876bbba3d3ff3e3d
nevrome <clemens@nevrome.de>
2019-07-03 14:39:10 +0200

    started to work on the filter form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,13 @@ class WelcomeController < ApplicationController
   def index
 
     #### update session ####
-    if params[:query_lat_start].present?
-      session[:query_lat_start] = params[:query_lat_start]
-    end
-    if params[:query_lat_stop].present?
-      session[:query_lat_stop] = params[:query_lat_stop]
+
+    # site name
+    if params[:query_site_name].present?
+      session[:query_site_name] = params[:query_site_name]
     end
 
+    # lasso
     if params[:spatial_lasso_selection].present?
       spatial_lasso_selection = Array.new;
       unless params[:spatial_lasso_selection].nil?
@@ -22,6 +22,7 @@ class WelcomeController < ApplicationController
       session[:spatial_lasso_selection] = nil;
     end
 
+    ##### select data #####
 		@selected_measurements = Measurement.joins(
       sample: {arch_object: [{site: [:site_type, :country]}, {on_site_object_position: :feature_type}, :material, :species]}
     ).select(
@@ -38,21 +39,24 @@ class WelcomeController < ApplicationController
       materials.name as material,
       (species.family || ' ' || species.genus || ' ' || species.species  || ' ' || species.subspecies) as species
       "
-    ).where(
-      "lat >= ? AND lat <= ?",
-      session[:query_lat_start],
-      session[:query_lat_stop],
     ).all
 
-    # https://github.com/jbox-web/ajax-datatables-rails/issues/246
-    params["columns"] ||= { "0" => {"data" => "" } }
-    params["length"]  ||= -1
+    unless session[:query_site_name].nil?
+      @selected_measurements = @selected_measurements.where(
+        "sites.name = ?", session[:query_site_name]
+      ).all
+    end
 
     unless session[:spatial_lasso_selection].nil?
        @selected_measurements = @selected_measurements.where(
         "measurements.id IN (?)", session[:spatial_lasso_selection]
        ).all
     end
+
+    #### provide data ####
+    # https://github.com/jbox-web/ajax-datatables-rails/issues/246
+    params["columns"] ||= { "0" => {"data" => "" } }
+    params["length"]  ||= -1
 
 		gon.selected_measurements = @selected_measurements.to_json
 
