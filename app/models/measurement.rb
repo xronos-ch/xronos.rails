5d8848ddb1c0cf57803f5a369f4b359e5de5d0e2
nevrome <clemens@nevrome.de>
2019-07-16 14:23:11 +0200

    more columns for download

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -5,9 +5,11 @@ class Measurement < ApplicationRecord
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << ["labnr", "site", "site_type"]
+      csv << ["labnr", "site", "site_type", "lat", "lng", "country", "feature", "material", "species"]
       all.each do |item|
-        csv << [item.labnr] + [item.site] + [item.site_type]
+        csv << [item.labnr] + [item.site] + [item.site_type] +
+            [item.lat] + [item.lng] + [item.country] + [item.feature] +
+            [item.material] + [item.species]
       end
     end
   end
