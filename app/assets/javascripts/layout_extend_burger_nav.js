535f40907108325b56d406ac058ad882c4862d09
nevrome <clemens@nevrome.de>
2019-07-17 17:09:46 +0200

    made extended position of main area persistend

--- app/assets/javascripts/layout_extend_burger_nav.js
+++ app/assets/javascripts/layout_extend_burger_nav.js
@@ -2,13 +2,22 @@ document.addEventListener('DOMContentLoaded',function(){
     const burger_nav = document.querySelector(".burger_nav");
     const btn = burger_nav.querySelector("#burger_nav_extend");
     const main = burger_nav.querySelector("main");
+
+    if (sessionStorage.getItem("burger_nav_big") === "1" && burger_nav.className.indexOf("big") === -1) {
+        burger_nav.classList.add("big");
+        main.classList.add("big");
+    }
+
     btn.addEventListener("click", evt => {
-        if (burger_nav.className.indexOf("big") === -1) {
+        if (sessionStorage.getItem("burger_nav_big") === "0" || sessionStorage.getItem("burger_nav_big") == null) {
+            sessionStorage.setItem("burger_nav_big", "1");
             burger_nav.classList.add("big");
             main.classList.add("big");
         } else {
+            sessionStorage.setItem("burger_nav_big", "0");
             burger_nav.classList.remove("big");
             main.classList.remove("big");
         }
     });
+
 });
