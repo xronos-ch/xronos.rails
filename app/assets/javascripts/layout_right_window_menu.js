756f5e8641e2ad5de7639327ba820d7581428c3e
nevrome <clemens@nevrome.de>
2019-07-22 10:42:50 +0200

    simplified user interface -- replaced draggable with another foldable window

---
+++ app/assets/javascripts/layout_right_window_menu.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const right_window_menu = document.querySelector(".right_window_menu");
+    const btn = right_window_menu.querySelector(".right_window_nav-tgl");
+    btn.addEventListener("click", evt => {
+        if (right_window_menu.className.indexOf("active") === -1) {
+            right_window_menu.classList.add("active");
+        } else {
+            right_window_menu.classList.remove("active");
+        }
+    });
+});
