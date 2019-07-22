8b9921d1b70e3c088bcdb0b6341da54fd5ca5342
nevrome <clemens@nevrome.de>
2019-07-22 11:04:25 +0200

    further simplificaion of main layout stylesheet

--- app/assets/javascripts/layout_left_window_menu.js
+++ app/assets/javascripts/layout_left_window_menu.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
 	const left_window_menu = document.querySelector(".left_window_menu");
-	const btn = left_window_menu.querySelector(".left_window_nav-tgl");
+	const btn = left_window_menu.querySelector("#left_window_nav_general");
 	btn.addEventListener("click", evt => {
 		if (left_window_menu.className.indexOf("active") === -1) {
 			left_window_menu.classList.add("active");
