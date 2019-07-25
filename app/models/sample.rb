95a72e2befcf707e8b817cb7a682893baf614d90
Clemens Schmid <clemens@nevrome.de>
2019-07-25 13:43:28 +0200

    work on nested validation

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,5 +1,6 @@
 class Sample < ApplicationRecord
   has_many :measurements, inverse_of: :sample
   accepts_nested_attributes_for :measurements, reject_if: :all_blank, allow_destroy: true
+  validates_associated :measurements
 	belongs_to :arch_object
 end
