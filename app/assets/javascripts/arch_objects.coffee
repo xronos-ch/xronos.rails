6447bce148e970530c14a429a2ceddeb66b2a2e0
Clemens Schmid <clemens@nevrome.de>
2019-07-25 15:24:58 +0200

    made observed select form field behaviour available everywhere

--- app/assets/javascripts/arch_objects.coffee
+++ app/assets/javascripts/arch_objects.coffee
@@ -1,23 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-
-jQuery ->
-  update_subfields = (object) ->
-    this_object = jQuery(object)
-    this_value = this_object.val()
-    this_next_div = this_object.parent().next(".field")
-    if(this_value=="")
-      this_next_div.show()
-      this_next_div.find("input").each ->
-        $(this).prop('disabled', false)
-    else
-      this_next_div.hide()
-      this_next_div.find("input").each ->
-        $(this).prop('disabled', true)
-
-  $('.observed_select').change ->
-    update_subfields(this)
-
-  $('.observed_select').each ->
-    update_subfields(this)
---
+++ app/assets/javascripts/layout_form_observed_select.js
@@ -0,0 +1,29 @@
+(function() {
+  jQuery(function() {
+    var update_subfields;
+    update_subfields = function(object) {
+      var this_next_div, this_object, this_value;
+      this_object = jQuery(object);
+      this_value = this_object.val();
+      this_next_div = this_object.parent().next(".field");
+      if (this_value === "") {
+        this_next_div.show();
+        return this_next_div.find("input").each(function() {
+          return $(this).prop('disabled', false);
+        });
+      } else {
+        this_next_div.hide();
+        return this_next_div.find("input").each(function() {
+          return $(this).prop('disabled', true);
+        });
+      }
+    };
+    $('.observed_select').change(function() {
+      return update_subfields(this);
+    });
+    return $('.observed_select').each(function() {
+      return update_subfields(this);
+    });
+  });
+
+}).call(this);
