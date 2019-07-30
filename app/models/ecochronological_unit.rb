8f5d0c4b35e99063c7fbf145c77b628497ff404a
Clemens Schmid <clemens@nevrome.de>
2019-07-30 11:28:45 +0200

    fields in forms for parent-child relation

--- app/models/ecochronological_unit.rb
+++ app/models/ecochronological_unit.rb
@@ -1,6 +1,6 @@
 class EcochronologicalUnit < ApplicationRecord
   validates :name, presence: true
   has_and_belongs_to_many :site_phases
-  belongs_to :parent, class_name: "EcochronologicalUnit"
+  belongs_to :parent, class_name: "EcochronologicalUnit", optional: true
   has_many :children, class_name: "EcochronologicalUnit", foreign_key: "parent_id"
 end
--- app/models/typochronological_unit.rb
+++ app/models/typochronological_unit.rb
@@ -1,6 +1,6 @@
 class TypochronologicalUnit < ApplicationRecord
   validates :name, presence: true
   has_and_belongs_to_many :site_phases
-  belongs_to :parent, class_name: "TypochronologicalUnit"
+  belongs_to :parent, class_name: "TypochronologicalUnit", optional: true
   has_many :children, class_name: "TypochronologicalUnit", foreign_key: "parent_id"
 end
