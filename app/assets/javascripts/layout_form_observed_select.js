b607a0f67e35f0d4e797ad168a87eaf153ab37ae
Clemens Schmid <clemens@nevrome.de>
2019-07-31 14:26:12 +0200

    code layout

--- app/assets/javascripts/layout_form_observed_select.js
+++ app/assets/javascripts/layout_form_observed_select.js
@@ -1,11 +1,14 @@
 (function() {
   jQuery(function() {
+
     var update_subfields;
+
     update_subfields = function(object) {
       var this_next_div, this_object, this_value;
       this_object = jQuery(object);
       this_value = this_object.val();
       this_next_div = this_object.parent().next(".field_master");
+
       if (this_value === "") {
         this_next_div.show();
         return this_next_div.find("input").each(function() {
@@ -17,13 +20,16 @@
           return $(this).prop('disabled', true);
         });
       }
+
     };
+
     $('.observed_select').change(function() {
       return update_subfields(this);
     });
     return $('.observed_select').each(function() {
       return update_subfields(this);
     });
+
   });
 
 }).call(this);
