6d464f7c4276ec98cc22b8f450e87ea1af5b33a8
Clemens Schmid <clemens@nevrome.de>
2019-07-31 15:45:49 +0200

    added some javascript to provide an edit link for dropdown selections

--- app/assets/javascripts/layout_form_observed_select.js
+++ app/assets/javascripts/layout_form_observed_select.js
@@ -4,18 +4,21 @@
     var update_subfields;
 
     update_subfields = function(object) {
-      var this_next_div, this_object, this_value;
+      var this_next_div, this_object, this_value, this_edit_link;
       this_object = jQuery(object);
       this_value = this_object.val();
       this_next_div = this_object.parent().next(".field_master");
+      this_edit_link = this_object.next(".observed_select_edit_link");
 
       if (this_value === "") {
         this_next_div.show();
+        this_edit_link.hide()
         return this_next_div.find("input").each(function() {
           return $(this).prop('disabled', false);
         });
       } else {
         this_next_div.hide();
+        this_edit_link.show()
         return this_next_div.find("input").each(function() {
           return $(this).prop('disabled', true);
         });
