4671e41abc32cd04fbb24606c3cee222587cca5b
Clemens Schmid <clemens@nevrome.de>
2019-08-14 14:09:01 +0200

    changed query layout

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -133,7 +133,28 @@ class DataController < ApplicationController
     ##### select data #####
 
     # general dataset preparation
-    @all_measurements = Measurement.left_joins( :c14_measurement, :references, :lab, sample: {arch_object: [{site_phase: [:site_type, {site: [:country, :fell_phases]}, :periods, :typochronological_units, :ecochronological_units]}, {on_site_object_position: :feature_type}, :material, :species]} ).select(
+    @all_measurements = Measurement.left_joins(
+      :c14_measurement,
+      :references,
+      :lab,
+      sample: {
+        arch_object: [{
+          site_phase: [
+          :site_type, {
+            site: [:country, :fell_phases]
+          },
+          :periods,
+          :typochronological_units,
+          :ecochronological_units
+          ]
+        }, {
+          on_site_object_position: :feature_type
+        },
+        :material,
+        :species
+        ]
+      } 
+    ).select(
       "
       arch_objects.id as arch_object_id,
       measurements.id as measurement_id,
