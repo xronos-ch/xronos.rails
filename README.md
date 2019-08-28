ea80a37677470f287366bc4eb06098495dcd6f4c
nevrome <clemens@nevrome.de>
2019-08-28 15:14:24 +0200

    simplified README and some early changes in the R code

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 [![Travis-CI Build
 Status](https://travis-ci.com/nevrome/xronos.rails.svg?token=vxsQ9RjxoGASGtX4Q8jc&branch=master)](https://travis-ci.com/nevrome/xronos.rails)
 
-# README
 
-Ideas for the Deployment procedure:
-
-1. Building the app image on dockerhub
-2. Running docker compose on the server
-
-The compose yaml has to be changed for this setup: Instead of building the app container, the already build image has to be downloaded. Environment variables in docker_env_variables + master.key have to be available when the entrypoint script runs. 
--- R_add_data_to_db/experiments.R
+++ R_add_data_to_db/experiments.R
@@ -1,37 +1,12 @@
+source("helper_functions.R")
+
 #### data preparation #### 
 
 imp <- c14bazAAR::get_aDRAC()
 
 imp %<>% c14bazAAR::calibrate(choices = "calprobdistr")
 
-simple_cal_list <- pbapply::pblapply(imp$calprobdistr, function(x) {
-  if (nrow(x) == 0) {
-    return(data.frame(bp = NA, std = NA))
-  }
-  middle <- which.min(abs(cumsum(x$density) - 0.5))
-  res_sum <- 0
-  i <- 1
-  while (res_sum < 0.95) {
-    start <- middle - i
-    stop <- middle + i
-    if (start <= 0) {
-      start <- 1
-    }
-    if (stop > nrow(x)) {
-      stop <- nrow(x)
-    }
-    res_sum <- sum(x$density[start:stop], na.rm = T)  
-    i <- i + 1
-  }
-  return(data.frame(bp = x$calage[middle], std = i))
-})
-
-simple_cal <- do.call(rbind, simple_cal_list)
-
-imp %<>% dplyr::mutate(
-  cal_bp = simple_cal$bp,
-  cal_std = simple_cal$std
-) 
+imp %<>% add_simple_cal()
 
 imp %<>% c14bazAAR::finalize_country_name()
 
