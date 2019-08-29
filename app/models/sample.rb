56e00d909dde01b91fd1a6dbbd592ac5dd37591f
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2019-08-29 17:16:05 +0200

    resolved observer problem with hidden fields and selects in layout_form_observed_select.js and _form.html.erb; added dependent: :destroy for samples in relation to measurments

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,5 +1,5 @@
 class Sample < ApplicationRecord
-  has_many :measurements, inverse_of: :sample
+  has_many :measurements, inverse_of: :sample, :dependent => :destroy
   accepts_nested_attributes_for :measurements, reject_if: :all_blank, allow_destroy: true
   validates_associated :measurements
 	belongs_to :arch_object
