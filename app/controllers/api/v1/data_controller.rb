4831615bee206cfac6c8b0dc67383262e9438d09
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2019-08-30 08:59:55 +0200

    finalized api draft

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class DataController < ApplicationController
-      prepend_view_path Rails.root.join("app", "views", "v1", "data")
-
       respond_to :json
 
       def index
