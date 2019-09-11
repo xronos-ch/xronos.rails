083f902bb34a3689d928bd727ce8e9a8cf1bf6b9
Clemens Schmid <clemens@nevrome.de>
2019-09-11 18:14:33 +0200

    experiments with controller tests

---
+++ test/controllers/countries_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class CountriesControllerTest < ActionDispatch::IntegrationTest
+#  include Devise::TestHelpers
+
+  setup do
+    @country = FactoryBot.create(:country)
+#    @admin =
+  end
+
+  test "should get index" do
+    get countries_url
+    assert_response :success
+  end
+
+#  test "should get new" do
+#    get new_country_url
+#    assert_response :success
+#  end
+
+#  test "should create country" do
+#    assert_difference('Country.count') do
+#      post countries_url, params: { country: {  } }
+#    end
+
+#    assert_redirected_to country_url(Country.last)
+#  end
+
+  test "should show country" do
+    get country_url(@country)
+    assert_response :success
+  end
+
+#  test "should get edit" do
+#    get edit_country_url(@country)
+#    assert_response :success
+#  end
+
+#  test "should update country" do
+#    patch country_url(@country), params: { country: {  } }
+#    assert_redirected_to country_url(@country)
+#  end
+
+#  test "should destroy country" do
+#    assert_difference('Country.count', -1) do
+#      delete country_url(@country)
+#    end
+
+#    assert_redirected_to countries_url
+#  end
+end
