32c8e0858b1e3174f5b26e0ab37eee2f9a89ff1e
Martin Hinz <martin.hinz@iaw.unibe.ch>
2021-08-01 12:55:52 +0200

    made migration of country sequence dependend on postgres

--- db/migrate/20210731164244_add_sequence_to_country_id.rb
+++ db/migrate/20210731164244_add_sequence_to_country_id.rb
@@ -2,22 +2,26 @@ class AddSequenceToCountryId < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up do
-        # add a sequence
-        execute <<-SQL
-          CREATE sequence countries_id_seq;
-          SELECT setval('countries_id_seq', (SELECT MAX(id) FROM countries));
-          ALTER TABLE countries
-            ALTER id
-              SET DEFAULT nextval('countries_id_seq');
-        SQL
+        if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+          # add a sequence
+          execute <<-SQL
+            CREATE sequence countries_id_seq;
+            SELECT setval('countries_id_seq', (SELECT MAX(id) FROM countries));
+            ALTER TABLE countries
+              ALTER id
+                SET DEFAULT nextval('countries_id_seq');
+              SQL
+        end
       end
       dir.down do
-        execute <<-SQL
-          ALTER TABLE countries
-            ALTER id
-              SET DEFAULT NULL;
-          DROP sequence countries_id_seq;
-        SQL
+        if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+          execute <<-SQL
+            ALTER TABLE countries
+              ALTER id
+                SET DEFAULT NULL;
+                DROP sequence countries_id_seq;
+              SQL
+        end
       end
     end
   end
