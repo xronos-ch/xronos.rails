45f42da9a120e4e454ac481302066228411a011d
Joe Roe <joe@joeroe.io>
2021-10-06 16:05:50 +0200

    Switch all new/edit forms to BS5 builder

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -81,4 +81,10 @@ module FormsHelper
     end
 
   end
+
+  def bs5_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &block)
+    options.merge! builder: BS5FormBuilder
+    form_with model: model, scope: scope, url: url, format: format, **options, &block
+  end
+
 end
