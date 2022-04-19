da86a7067e3479462b7b01c1665272b89b13ed0f
Joe Roe <git@joeroe.io>
2022-04-19 14:19:21 +0200

    Redirect to last page on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :set_paper_trail_whodunnit
 
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
-  
+
+  # Redirect to last page on sign in
+  # https://github.com/heartcombo/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
   # access management error handling (messages)
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
@@ -24,4 +31,14 @@ class ApplicationController < ActionController::Base
 
   # default_form_builder FormHelper::BS5FormBuilder
     
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
 end
