4ae5afd7d3d49242ca0a11b35f01b6794c8bd046
Joe Roe <git@joeroe.io>
2022-05-25 14:55:47 +0200

    Basic forms for c14, context, sample

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,6 @@
 class Material < ApplicationRecord
+  default_scope { order(name: :asc) }
+
   has_many :samples, inverse_of: :material
   validates :name, presence: true
 end
--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,4 +1,5 @@
 class Taxon < ApplicationRecord
+  default_scope { order(name: :asc) }
   validates :name, presence: true
 
   has_many :samples
