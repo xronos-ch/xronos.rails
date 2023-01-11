14f3e27fe07bdee0abde875295a91afc7aef0031
Joe Roe <git@joeroe.io>
2023-01-11 13:46:36 +0100

    Add button to regenerate slug

--- app/javascript/controllers/article_editor_controller.js
+++ app/javascript/controllers/article_editor_controller.js
@@ -9,16 +9,25 @@ export default class extends Controller {
 		if (this.slugTarget.value != '') {
 			this.slugHasBeenTouched = true
 		}
+		this.addSlugifyButton()
   }
+
+	disconnect() {
+		this.removeSlugifyButton()
+	}
+
+	inputTitle() {
+		if (!this.slugHasBeenTouched) {
+			this.updateSlug()
+		}
+	}
 
 	touchSlug() {
 		this.slugHasBeenTouched = true
 	}
 
 	updateSlug() {
-		if (!this.slugHasBeenTouched) {
-			this.slugTarget.value = this.slugify(this.titleTarget.value)
-		}
+		this.slugTarget.value = this.slugify(this.titleTarget.value)
 	}
 
 	// https://www.30secondsofcode.org/js/s/slugify
@@ -29,5 +38,20 @@ export default class extends Controller {
 			.replace(/[^\w\s-]/g, '')
 			.replace(/[\s_-]+/g, '-')
 			.replace(/^-+|-+$/g, '')
+	}
+
+	addSlugifyButton() {
+		let slugifyBtn = document.createElement("button")
+		slugifyBtn.type = "button"
+		slugifyBtn.title = "Regenerate slug from title"
+		slugifyBtn.classList.add('btn')
+		slugifyBtn.classList.add('btn-outline-info')
+		slugifyBtn.dataset.action = "article-editor#updateSlug"
+		slugifyBtn.innerHTML = '<i class="fa fa-refresh"></i><span class="visually-hidden">Regenerate slug</span>'
+		this.slugTarget.parentElement.append(slugifyBtn)
+	}
+
+	removeSlugifyButton() {
+		// TO DO
 	}
 }
