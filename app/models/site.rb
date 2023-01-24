eb590856b586a8eea07b46cca9ee1b1118668c4c
Joe Roe <joe@joeroe.io>
2023-01-24 15:07:10 +0100

    Don't use the options for now

--- app/models/site.rb
+++ app/models/site.rb
@@ -22,7 +22,7 @@ class Site < ApplicationRecord
   include Versioned
   include Supersedable
   include Duplicable
-  duplicable :name, :lat, :lng, country_code: nil
+  duplicable :name, :lat, :lng, :country_code
 
   include PgSearch::Model
   pg_search_scope :search, 
