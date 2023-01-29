34399f8550ab87adc8863b5e6998bd63850715f7
Joe Roe <git@joeroe.io>
2023-01-29 09:38:52 +0100

    Change wording of default search placeholder slightly

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,6 +1,6 @@
 module SearchesHelper
   def random_search
     random = rand(PgSearch::Document.count)
-    random >= 1 ? PgSearch::Document.offset(random).first.content : "Site name or Lab Code"
+    random >= 1 ? PgSearch::Document.offset(random).first.content : "Site name, lab code, ..."
   end
 end
