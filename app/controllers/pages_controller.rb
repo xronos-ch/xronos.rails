7780e857b97ba9a3e9b09d5521a6a6fc06410285
Joe Roe <git@joeroe.io>
2023-03-11 10:11:44 +0100

    Only show published news posts on front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @news = Article.news_section.order(published_at: :desc).limit(5)
+    @news = Article.news_section.published.order(published_at: :desc).limit(5)
   end
 
 end
