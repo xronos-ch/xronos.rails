6c7ae7d4a22cb556f6cd5d86a81d6af205a06069
Martin Hinz <martin.hinz@iaw.unibe.ch>
2023-03-24 12:02:42 +0100

    added admin to user form (for admins)

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe "User Interactions", type: :request, js: true do
       @user_profile = UserProfile.create!(first_name: "Horst", last_name: "Hans", user: @user)
       @admin_user = FactoryBot.create(:user, :admin)
       @admin_user_profile = UserProfile.create!(first_name: "Willi", last_name: "Wichtig", user: @admin_user)
-      
      end
      
     it "has a Sign in Button" do
@@ -30,7 +29,17 @@ RSpec.describe "User Interactions", type: :request, js: true do
       visit 'user_profile'
       expect(page).to have_content(@user.email)
     end
+  end
+
+  describe "capabilities" do
     
+    before(:each) do
+      @user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
+      @user_profile = UserProfile.create!(first_name: "Horst", last_name: "Hans", user: @user)
+      @admin_user = FactoryBot.create(:user, :admin)
+      @admin_user_profile = UserProfile.create!(first_name: "Willi", last_name: "Wichtig", user: @admin_user)
+     end
+     
     it "blocks normal users from seeing the user profile index" do
       sign_in @user
       expect do
@@ -42,6 +51,20 @@ RSpec.describe "User Interactions", type: :request, js: true do
     it "allows admin users to see the user profile index" do
       sign_in @admin_user
       expect{visit 'user_profiles'}.not_to raise_error
+    end
+    
+    it "hides admin checkbox from normal users" do
+      sign_in @user
+      visit 'user_profile'
+      click_link('Edit')
+      expect(page).not_to have_field("user_profile_user_attributes_admin")
+    end
+    
+    it "shows admin checkbox to admin users" do
+      sign_in @admin_user
+      visit 'user_profile'
+      click_link('Edit')
+      expect(page).to have_field("user_profile_user_attributes_admin")
     end
   end
 end
