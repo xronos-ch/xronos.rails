25fdba2cc9d9c230886700f6dd1ca0a2646f5363
Martin Hinz <martin.hinz@iaw.unibe.ch>
2023-03-24 14:58:53 +0100

    changed settings for headless chrome selenium driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,22 +74,19 @@ RSpec.configure do |config|
   
   Capybara.server = :puma, { Silent: true } # To clean up your test output
   
-  Capybara.register_driver :selenium_chrome_headless do |app|
-    version = Capybara::Selenium::Driver.load_selenium
-    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
-    browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument("--headless=new")
-      opts.add_argument("--disable-gpu") if Gem.win_platform?
-      # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-      opts.add_argument("--disable-site-isolation-trials")
-      opts.add_preference("download.default_directory", Capybara.save_path)
-      opts.add_preference(:download, default_directory: Capybara.save_path)
-    end
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new(
+      args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+    )
 
-    Capybara::Selenium::Driver.new(app, **{ browser: :chrome, options_key => browser_options })
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+    )
   end
   
-  Capybara.default_driver = :selenium_chrome_headless
+  Capybara.default_driver = :headless_chrome
   
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
