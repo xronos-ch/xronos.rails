19d5af1558b8158c33ecf6354774623be5b942dd
Joe Roe <git@joeroe.io>
2023-03-24 15:49:53 +0100

    Use simple icons for Wikimedia logos

--- app/helpers/simple_icons_helper.rb
+++ app/helpers/simple_icons_helper.rb
@@ -11,12 +11,11 @@ module SimpleIconsHelper
   def embedded_svg(filename, options = {})
     assets = Rails.application.assets
 
-    asset = assets.load_path.find(filename)
+    asset = assets.find_asset(filename)
 
     if asset
-      #file = asset.source.force_encoding("UTF-8")
-      doc = Nokogiri::HTML::DocumentFragment.parse asset.content
-
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
       svg = doc.at_css "svg"
       options.each {|attr, value| svg[attr.to_s] = value}
       #svg["class"] = options[:class] if options[:class].present?
