93eec204ef998525a987f44a744a878cf116fa55
Joe Roe <git@joeroe.io>
2023-03-24 14:45:46 +0100

    Add Wikidata and site links to site show view

--- app/models/wikidata_item.rb
+++ app/models/wikidata_item.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class WikidataItem
   attr_reader :wikipedia_extract
 
@@ -47,29 +46,4 @@ class WikidataItem
     qid = qid.to_s
     qid = "Q" + qid unless qid.starts_with?("Q")
   end
-
-=======
-# == Schema Information
-#
-# Table name: wikidata_items
-#
-#  id                 :bigint           not null, primary key
-#  qid                :integer
-#  wikidata_link_type :string
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  wikidata_link_id   :bigint
-#
-# Indexes
-#
-#  index_wikidata_items_on_qid                                      (qid)
-#  index_wikidata_items_on_wikidata_link                            (wikidata_link_type,wikidata_link_id)
-#  index_wikidata_items_on_wikidata_link_type_and_qid               (wikidata_link_type,qid)
-#  index_wikidata_items_on_wikidata_link_type_and_wikidata_link_id  (wikidata_link_type,wikidata_link_id)
-#
-class WikidataItem < ApplicationRecord
-
-  belongs_to :wikidata_link, polymorphic: true
-
->>>>>>> b23be7c (Create WikidataItem and add to sites)
 end
