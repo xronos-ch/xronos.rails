be7d7d1bad43eca2b724f20ea46788601ac8923a
Joe Roe <git@joeroe.io>
2023-03-26 08:24:35 +0200

    Rename WikidataItem to WikidataLink

--- app/models/site.rb
+++ app/models/site.rb
@@ -25,7 +25,7 @@ class Site < ApplicationRecord
   has_many :typos, through: :contexts
   has_many :citations, as: :citing
   has_many :references, through: :citations
-  has_one :wikidata_item, as: :wikidata_link
+  has_one :wikidata_link, as: :wikidata_linkable
   has_and_belongs_to_many :site_types, optional: true
 
   composed_of :coordinates, mapping: [%w(lng longitude), %w(lat latitude)], 
