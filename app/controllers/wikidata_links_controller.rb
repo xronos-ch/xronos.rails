452ccdd58c3f87dda332594f1867898a3be15c3d
Joe Roe <joe@joeroe.io>
2023-03-28 15:55:13 +0200

    Add Wikipedia extracts to site view

--- app/controllers/wikidata_links_controller.rb
+++ app/controllers/wikidata_links_controller.rb
@@ -3,6 +3,10 @@ class WikidataLinksController < ApplicationController
 
   def show
     @wikidata_link.request_item
+    if @wikidata_link.item.sitelink_title("enwiki").present?
+      @wikidata_link.item.request_wikipedia_extract
+    end
+
     render partial: "wikidata_link"
   end
 end
---
+++ app/models/wikipedia_extract.rb
@@ -0,0 +1,25 @@
+class WikipediaExtract
+  include ActiveModel::Conversion
+
+  def initialize(title, lang = "en")
+    @title = title
+    @lang = lang
+    opts = { prop: ["extracts", "fullurl"], exintro: true, explaintext: true } # return first extract, plain text
+    @page = wikipedia_api(lang).find(title, opts)
+  end
+
+  def text
+    @page.summary
+  end
+
+  def url
+    "https://#{@lang}.wikipedia.org/wiki/#{ERB::Util.url_encode @title}"
+  end
+
+  protected
+
+  def wikipedia_api(lang = "en")
+    config = Wikipedia::Configuration.new(domain: "#{lang}.wikipedia.org")
+    return Wikipedia::Client.new(config)
+  end
+end
