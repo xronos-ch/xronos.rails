8df4ec0e5a758eee09fcb3275bedf3baab06ea4f
Joe Roe <joe@joeroe.io>
2023-04-03 11:44:46 +0200

    Preserve GET params in issue index filter form. #245

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -10,13 +10,14 @@ class IssuesController < ApplicationController
 
   private
 
+  def issues
+    # override, e.g. Site.issues
+  end
+
   def issue_param
     issue = params.fetch(:issue, nil)
     return issue if issue.present? and issue.in?(issues.to_s)
   end
 
-  def permitted_issues
-    # override, e.g. Site.issues
-  end
 
 end
