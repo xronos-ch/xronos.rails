8bc234be36801bae28d5ceb96868e43e14987705
Joe Roe <joe@joeroe.io>
2023-04-17 14:30:30 +0200

    Use range slider to filter uncal bp #288
    With a no-JS fallback.

---
+++ app/javascript/controllers/slider_controller.js
@@ -0,0 +1,70 @@
+import { Controller } from "@hotwired/stimulus"
+import noUiSlider from "nouislider";
+import "nouislider/dist/nouislider.css";
+import wNumb from "wnumb";
+
+// Connects to data-controller="slider"
+export default class extends Controller {
+	static targets = [ "minInput", "maxInput", "slider" ]
+	static values = {
+		step: Number,
+		direction: String,
+		formatter: String,
+		range: Object,
+		density: Number
+	}
+
+  connect() {
+		this.element.classList.add("has-slider");
+
+		var minInput = this.minInputTarget;
+		var maxInput = this.maxInputTarget;
+
+		// slider parameters
+		var step = (this.stepValue != 0 ? this.stepValue : 1);
+		var direction = (this.directionValue != '' ? this.directionValue : 'ltr');
+		var range = (this.hasRangeValue ? this.rangeValue : this.defaultRange());
+		var density = (this.densityValue != 0 ? this.densityValue : 1);
+
+		this.slider = noUiSlider.create(this.sliderTarget, {
+			start: [minInput.value, maxInput.value],
+			connect: true,
+			step: step,
+			direction: direction,
+			tooltips: this.formatter(),
+			range: range,
+			pips: {
+				mode: 'range',
+				density: density
+			}
+		});
+
+		this.slider.on('update', function(values, handle) {
+			minInput.value = values[0];
+			maxInput.value = values[1];
+		})
+	}
+
+	disconnect() {
+
+	}
+
+	formatter() {
+		var formatters = {
+			"integer": wNumb({ decimal: 0 }),
+			"uncalBP": wNumb({
+				decimal: 0,
+				thousands: "&thinsp;",
+				suffix: " uncal BP"
+			})
+		}
+		return formatters[this.formatterValue];
+	}
+
+	defaultRange() {
+		return { 
+			'min': Number(this.minInputTarget.min), 
+			'max': Number(this.minInputTarget.max) 
+		};
+	}
+}
