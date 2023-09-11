6734ba83ffc415f5efbb04c89563cc87c228a24e
Joe Roe <joe@joeroe.io>
2023-09-11 12:07:46 +0200

    Move packs/application.js to application.js
    Disabling ActionCable for now: https://github.com/xronos-ch/xronos.rails/issues/301

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+
+import * as ActiveStorage from "@rails/activestorage"
+//import "./channels" // https://github.com/xronos-ch/xronos.rails/issues/301
+
+// hotwire (turbo+stimulus)
+import "@hotwired/turbo-rails"
 import "./controllers"
+
+//fonts
+require("@fontsource/inter")
+require("@fontsource/raleway")
+
+ActiveStorage.start()
