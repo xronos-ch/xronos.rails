8034933e033812406ba6cbec896c3b96f04a3964
Martin Hinz <martin.hinz@unibe.ch>
2025-12-10 12:06:00 +0100

    prevent eager loading on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,10 @@
 class PagesController < ApplicationController
-
   def home
     @news = Article
       .news_section
       .published
+      .includes(user: :user_profile, splash_attachment: :blob)
       .order(published_at: :desc)
       .limit(5)
-      .includes([splash_attachment: :blob])
-      .includes(:user)
   end
-
 end
