da02921811e0f0ac5893f3b4f47025093e91b04b
Martin Hinz <martin.hinz@unibe.ch>
2026-05-27 15:04:02 +0200

    issues controller for contexts

---
+++ app/controllers/issues/contexts_controller.rb
@@ -0,0 +1,47 @@
+class Issues::ContextsController < IssuesController
+  load_and_authorize_resource
+
+  # GET /issues/contexts/:issue
+  def index
+    @contexts = if issue_param.present?
+                  ::Context.send(issue_param)
+                else
+                  ::Context.all
+                end
+
+    if params.has_key?(:search)
+      @contexts = @contexts.search(params[:search])
+    end
+
+    if params[:suggestions].present?
+      @contexts = @contexts.merge(::Context.with_functional_classification_suggestions)
+    end
+
+    @contexts = @contexts.includes(
+      site: :site_types,
+      functional_classifications: [
+        :functional_classification_category,
+        :functional_classification_confidence
+      ]
+    )
+
+    order =
+      if params.has_key?(:contexts_order_by)
+        { params[:contexts_order_by] => params.fetch(:contexts_order, "asc") }
+      else
+        :id
+      end
+
+    @contexts = @contexts.reorder(order)
+
+    respond_to do |format|
+      format.html { @pagy, @contexts = pagy(@contexts) }
+    end
+  end
+
+  private
+
+  def issues
+    ::Context.issues
+  end
+end
