b112100b4f168613b8153bd8bd18f695ec6e957c
Martin Hinz <martin.hinz@unibe.ch>
2026-05-27 15:03:36 +0200

    specs for functional classifications

---
+++ spec/models/functional_classification_spec.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: functional_classifications
+#
+#  id                                      :bigint           not null, primary key
+#  assignable_type                         :string           not null
+#  note                                    :text
+#  source                                  :string
+#  created_at                              :datetime         not null
+#  updated_at                              :datetime         not null
+#  assignable_id                           :bigint           not null
+#  functional_classification_category_id   :bigint           not null
+#  functional_classification_confidence_id :bigint           not null
+#
+# Indexes
+#
+#  idx_functional_classifications_unique_category             (assignable_type,assignable_id,functional_classification_category_id) UNIQUE
+#  idx_on_functional_classification_category_id_0cc23f287f    (functional_classification_category_id)
+#  idx_on_functional_classification_confidence_id_882617d1c0  (functional_classification_confidence_id)
+#  index_functional_classifications_on_assignable             (assignable_type,assignable_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (functional_classification_category_id => functional_classification_categories.id)
+#  fk_rails_...  (functional_classification_confidence_id => functional_classification_confidences.id)
+#
+require 'rails_helper'
+
+RSpec.describe FunctionalClassification, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
