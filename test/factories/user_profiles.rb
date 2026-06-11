8d76b945dfe0a5a59a08fe8bf95bfc64f7a374b6
Joe Roe <git@joeroe.io>
2026-06-11 15:00:48 +0200

    Add repeatable factory for users
    Previously it could only be used once as it had a hardcoded email

--- test/factories/user_profiles.rb
+++ test/factories/user_profiles.rb
@@ -20,12 +20,52 @@
 #
 #  fk_rails_...  (user_id => users.id)
 #
-
 FactoryBot.define do
   factory :user_profile do
-    full_name { Faker::Name.name }
-    orcid { '0000-0002-1825-0097' }
-    public_email { Faker::Internet.email }
-    url { Faker::Internet.url(host: "example.com") }
+    association :user
+
+    full_name { "Test User" }
+
+    sequence(:orcid) { |n| format("0000-0000-0000-%04d", n) }
+
+    public_email { user.email }
+
+    url { "https://example.com/profile" }
+
+    # --- Traits ---
+
+    trait :with_realistic_name do
+      sequence(:full_name) { |n| "User #{n}" }
+    end
+
+    trait :with_orcid do
+      # already default, but kept for clarity/override
+    end
+
+    trait :without_orcid do
+      orcid { nil }
+    end
+
+    trait :custom_email do
+      sequence(:public_email) { |n| "public#{n}@example.com" }
+    end
+
+    trait :no_public_email do
+      public_email { nil }
+    end
+
+    trait :with_url do
+      sequence(:url) { |n| "https://example.com/users/#{n}" }
+    end
+
+    trait :no_url do
+      url { nil }
+    end
+
+    trait :complete do
+      with_realistic_name
+      with_url
+    end
   end
 end
+
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -17,6 +17,29 @@
 #
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
-#
 FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "user#{n}@xronos.ch" }
+
+    password { "password" }
+    password_confirmation { "password" }
+
+    admin { false }
+
+    # Only include if validation requires it
+    passphrase { ENV["REGISTRATION_PASSPHRASE"] }
+
+    # --- Traits ---
+
+    trait :admin do
+      admin { true }
+    end
+
+    # --- Associations ---
+
+    after(:build) do |user|
+      user.user_profile ||= build(:user_profile, user: user)
+    end
+  end
 end
+
