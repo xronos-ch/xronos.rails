5bb4a4af67d49e29ecfe0e378306c15d0f525105
Joe Roe <git@joeroe.io>
2026-06-12 02:58:59 +0200

    Add OpenGraph metadata for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,25 +27,15 @@ class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :splash
 
-  # Workaround lack of named attachment variants in Rails <7
-  def splash_variant(variant)
-    return nil unless splash.present?
-    case variant
-    when :card
-      splash.variant(resize_to_fill: [1320, 160])
-    when :xs
-      splash.variant(resize_to_fill: [576, 320])
-    when :sm
-      splash.variant(resize_to_fill: [768, 320])
-    when :md
-      splash.variant(resize_to_fill: [992, 320])
-    when :lg
-      splash.variant(resize_to_fill: [1200, 320])
-    when :xl
-      splash.variant(resize_to_fill: [1400, 320])
-    when :xxl
-      splash.variant(resize_to_fill: [2500, 320])
-    end
+  has_one_attached :splash do |attachable|
+    attachable.variant :card, resize_to_fill: [1320, 160]
+    attachable.variant :xs,   resize_to_fill: [576, 320]
+    attachable.variant :sm,   resize_to_fill: [768, 320]
+    attachable.variant :md,   resize_to_fill: [992, 320]
+    attachable.variant :lg,   resize_to_fill: [1200, 320]
+    attachable.variant :xl,   resize_to_fill: [1400, 320]
+    attachable.variant :xxl,  resize_to_fill: [2500, 320]
+    attachable.variant :og, resize_to_fill: [1200, 630, { gravity: "center" }]
   end
 
   validates :title, presence: true
@@ -93,7 +83,7 @@ class Article < ApplicationRecord
 
   def author_name
     if about_section? or user.user_profile.blank? or user.user_profile.full_name.blank?
-      "XRONOS development team"
+      "XRONOS Core Team"
     else
       user.user_profile.full_name
     end
