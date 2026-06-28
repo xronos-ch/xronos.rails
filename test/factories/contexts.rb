95c5ea59bbf2da307b10c8e816de70857a54b204
Joe Roe <git@joeroe.io>
2026-06-28 12:38:05 +0200

    Make test context name deterministic

--- test/factories/contexts.rb
+++ test/factories/contexts.rb
@@ -20,12 +20,12 @@
 #
 
 FactoryBot.define do
-  
+
   factory :context do
-    name { Faker::Tea.unique.variety }
+    sequence(:name) { |n| "Context #{n}" }
     approx_start_time {Faker::Number.between(from: 3000, to: 4000)}
     approx_end_time {Faker::Number.between(from: 1000, to: 2000)}
     site
   end
-  
+
 end
