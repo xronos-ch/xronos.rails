58ecc30aeeb3a9d947657bb530ee407803b9d6cc
Joe Roe <git@joeroe.io>
2026-07-12 13:33:13 +0200

    Add controller tests for changelog methods

--- test/controllers/curate/recent_changes_controller_test.rb
+++ test/controllers/curate/recent_changes_controller_test.rb
@@ -13,7 +13,7 @@ class Curate::RecentChangesControllerTest < ActionDispatch::IntegrationTest # ru
     }
   )
 
-  test "index renders paginated changelog" do
+  test 'index renders paginated changelog' do
     sign_in create(:user, :admin)
 
     create_list(:site, 25)
@@ -21,16 +21,64 @@ class Curate::RecentChangesControllerTest < ActionDispatch::IntegrationTest # ru
     get curate_recent_changes_path
 
     assert_response :success
-    assert_includes response.body, "pagy"
-    assert_includes response.body, "series-nav"
+    assert_includes response.body, 'pagy'
+    assert_includes response.body, 'series-nav'
   end
 
-  test "index shows message when no changes" do
+  test 'index includes both PaperTrail versions and SupersessionEvents' do
+    sign_in create(:user, :admin)
+
+    with_versioning do
+      site = build(:site)
+      site.revision_comment = 'Created test site'
+      site.save!
+    end
+
+    superseded = create(:site)
+    canonical = create(:site)
+    SupersessionEvent.create!(
+      event_type: 'supersede',
+      superseded: superseded,
+      superseded_by: canonical,
+      whodunnit_user: create(:user)
+    )
+
+    get curate_recent_changes_path
+
+    assert_response :success
+    assert_includes response.body, 'Created test site'
+    assert_includes response.body, 'Superseded by'
+  end
+
+  test 'index paginates mixed changelog entries' do
+    sign_in create(:user, :admin)
+
+    with_versioning do
+      15.times { create(:site) }
+    end
+
+    12.times do
+      SupersessionEvent.create!(
+        event_type: 'supersede',
+        superseded: create(:site),
+        superseded_by: create(:site),
+        whodunnit_user: create(:user)
+      )
+    end
+
+    get curate_recent_changes_path, params: { page: 1 }
+
+    assert_response :success
+    assert_includes response.body, 'pagy'
+    assert_includes response.body, 'series-nav'
+  end
+
+  test 'index shows message when no changes' do
     sign_in create(:user, :admin)
 
     get curate_recent_changes_path
 
     assert_response :success
-    assert_includes response.body, "No recent changes"
+    assert_includes response.body, 'No recent changes'
   end
 end
